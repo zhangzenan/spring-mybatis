1b0fcb40114a45f12fe939f68762d04715a65480
zhangzenan <zhangzenan@yiguo.com>
2017-07-17 12:56:39 +0800

    测试

--- src/cn/itcast/ssm/mapper/UserMapper.java
+++ src/cn/itcast/ssm/mapper/UserMapper.java
@@ -1,12 +1,11 @@
 package cn.itcast.ssm.mapper;
 
-import java.util.List;
-
 import cn.itcast.ssm.po.User;
 
 public interface UserMapper {
 	
 	//根据id查询用户信息
 	public User findUserById(int id) throws Exception;
+	
 	
 }
